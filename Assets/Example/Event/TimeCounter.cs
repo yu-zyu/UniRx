e1a69162635f520d2b83aeee293b57860fd399f1
yu-zyu <siragaigarasi@gmail.com>
2021-08-15 10:53:19 +0900

    イベントの例を動かしてみた

---
+++ Assets/Example/Event/TimeCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 100からカウントダウンし値を通知するサンプル
+/// </summary>
+public class TimeCounter : MonoBehaviour
+{
+    /// <summary>
+    /// イベントハンドラ（イベントメッセージの型定義）
+    /// </summary>
+    public delegate void TimerEventHandler(int time);
+
+    /// <summary>
+    /// イベント
+    /// </summary>
+    public event TimerEventHandler OnTimeChanged;
+
+
+    void Start()
+    {
+        //タイマ起動
+        StartCoroutine(TimerCoroutine());
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        //100からカウントダウン
+        var time = 100;
+        while (time > 0)
+        {
+            time--;
+            //イベント通知
+            OnTimeChanged(time);
+
+            //1秒待つ
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Assets/Example/Event/TimerView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour
+{
+    //それぞれインスタンスはインスペクタビューから設定
+
+    [SerializeField] private TimeCounter timeCounter; 
+    [SerializeField] private Text counterText; //uGUIのText
+
+    void Start()
+    {
+        //タイマのカウンタが変化したイベントを受けてuGUI Textを更新する
+        timeCounter.OnTimeChanged += time => // =>は「ラムダ式」と呼ばれる匿名関数の記法
+        {
+            //現在のタイマ値をUIに反映する
+            counterText.text = time.ToString();
+        };
+    }
+}
