838b21d96a98d928db5f84062db371dcbd365eca
yu-zyu <siragaigarasi@gmail.com>
2021-08-15 11:42:32 +0900

    EventをUniRX化したバージョンを再現した

---
+++ Assets/Example/Event/UniRx/TimeCounterUniRx.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+using System; // .NET 4.xモードで動かす場合は必須
+
+/// <summary>
+/// 100からカウントダウンし、Debug.Logにその値を表示するサンプル
+/// </summary>
+public class TimeCounterUniRx : MonoBehaviour
+{
+    //イベントを発行する核となるインスタンス
+    private Subject<int> timerSubject = new Subject<int>();
+
+    //イベントの購読側だけを公開
+    public IObservable<int> OnTimeChanged
+    {
+        get { return timerSubject; }
+    } 
+
+    void Start()
+    {
+        StartCoroutine(TimerCoroutine());
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        //100からカウントダウン
+        var time = 100;
+        while (time > 0)
+        {
+            time--;
+
+            //イベントを発行
+            timerSubject.OnNext(time);
+
+            //1秒待つ
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Assets/Example/Event/UniRx/TimerViewUniRx.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+
+public class TimerViewUniRx : MonoBehaviour
+{
+    //それぞれインスタンスはインスペクタビューから設定
+
+    [SerializeField] private TimeCounterUniRx timeCounter;
+    [SerializeField] private Text counterText; //uGUIのText
+
+    void Start()
+    {
+        //タイマのカウンタが変化したイベントを受けてuGUI Textを更新する
+        timeCounter.OnTimeChanged.Subscribe(time =>
+        {
+            //現在のタイマ値をUIに反映する
+            counterText.text = time.ToString();
+        });
+    }
+}
