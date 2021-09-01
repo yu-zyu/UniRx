c5da2041543f943a68ddba4d711434b07852985a
yu-zyu <siragaigarasi@gmail.com>
2021-09-01 09:23:06 +0900

    operatorを実装してみた

---
+++ Assets/Example/Chapter2/2.1.3/Operator/OperatorTest.cs
@@ -0,0 +1,33 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section2.Operators
+{
+    public class OperatorTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var subject = new Subject<int>();
+
+            //そのままSubscribe
+            subject.Subscribe(x => Debug.Log("raw: " + x));
+
+            // 0 以下を除外してSubscribe
+            // Whereを間に挟む事で、条件を満たすメッセージのみを通過させる事ができる
+            subject
+                .Where(x => x > 0)
+                .Subscribe(x => Debug.Log("filter:" + x));
+
+            //メッセージ発行
+            subject.OnNext(1);
+            subject.OnNext(-1);
+            subject.OnNext(3);
+            subject.OnNext(0);
+
+            //終了
+            subject.OnCompleted();
+            subject.Dispose();
+        }
+    }
+}
