8f723b13d1bd975cfce4c5eaa4e4e03002e03748
yu-zyu <siragaigarasi@gmail.com>
2021-09-04 15:54:26 +0900

    複数回Subscribe()を実行する例を再現した

---
+++ Assets/Example/Chapter2/2.3/2.3.2/ExampleOfExecutingSubscribeMultipleTimes/ExampleOfExecutingSubscribeMultipleTimes.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class ExampleOfExecutingSubscribeMultipleTimes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var subject = new Subject<int>();
+
+        //発行された値を途中でログに出すObservable
+        //DoはOnNext()のたびに呼ばれる
+        var observable = subject.Do(x => Debug.Log("Do:" + x));
+
+        //2会Subscribeする
+        observable.Subscribe(x => Debug.Log("First subscribe:" + x));
+        observable.Subscribe(x => Debug.Log("Second subscribe:" + x));
+
+        subject.OnNext(1);
+        subject.OnCompleted();
+        subject.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
