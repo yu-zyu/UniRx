0703e921d94b9568d3c8aa113c35d80aca32d613
yu-zyu <siragaigarasi@gmail.com>
2021-09-04 15:26:56 +0900

    Subscribe()のタイミングと挙動２を再現した

---
+++ Assets/Example/Chapter2/2.3/SubscribeTimingAndBehavior/SubscribeTimingAndBehavior2.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class SubscribeTimingAndBehavior2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var subject = new Subject<string>();
+
+        //OnNextの内容をスペース区切りで連結し、最後の１つだけを出力するObservable
+        var appendStringObservable =
+            subject.Scan((previous, current) => previous + " " + current)
+            .Last();
+
+        //Subscribeされる前に値を発行する
+        subject.OnNext("I");
+        subject.OnNext("have");
+
+        //途中でSubscribeする
+        appendStringObservable.Subscribe(x => Debug.Log(x));
+
+        subject.OnNext("a");
+        subject.OnNext("pen");
+        subject.OnCompleted();
+        subject.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
