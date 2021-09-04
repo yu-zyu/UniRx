a17a94ac9890d406db4c41dbaba80d12d3bbf7e3
yu-zyu <siragaigarasi@gmail.com>
2021-09-04 16:24:11 +0900

    Subjectの代わりに専用のOperatorを使うを再現した

---
+++ Assets/Example/Chapter2/2.3/2.3.3/HotObservableAndColdObservable/HotConversionExampleUsingSubject/HotConversionExampleUsingSubject.cs
@@ -0,0 +1,44 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class HotConversionExampleUsingSubject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var originalSubject = new Subject<string>();
+
+        //OnNextの内容をスペース区切りで連結し、最後の1つだけを出力するObservable
+        var apendStringObservable =
+            originalSubject
+            .Scan((previous, current) => previous + " " + current)
+            .Last();
+
+        //連結用のSubject
+        var publishSubject = new Subject<string>();
+
+        //元のObsrvableに対して、PublishSubjectを使ってSubscribeを実行する
+        //この時点でappendStringObservableが動き始める
+        apendStringObservable.Subscribe(publishSubject);
+
+        originalSubject.OnNext("I");
+        originalSubject.OnNext("have");
+
+        //途中でpubleshSubjectのほうをSubscribeする
+        publishSubject.Subscribe(x => Debug.Log(x));
+
+        originalSubject.OnNext("a");
+        originalSubject.OnNext("pen");
+        originalSubject.OnCompleted();
+
+        //Hot変換解除
+        publishSubject.Dispose();
+        originalSubject.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Example/Chapter2/2.3/2.3.3/HotObservableAndColdObservable/UseDedicatedOperatorInsteadOfSubject/UseDedicatedOperatorInsteadOfSubject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class UseDedicatedOperatorInsteadOfSubject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var originalSubject = new Subject<string>();
+
+        //OnNextの内容をスペース区切りで連結し、最後の１つだけを出力するObservable
+        //IConnectableObservable<string>になっている
+        IConnectableObservable<string> appendStringObservable =
+            originalSubject
+            .Scan((previous, current) => previous + " " + current)
+            .Last()
+            .Publish(); // Hot変換するOperator
+
+        //IconnectableObservable.Connect()を呼び出すと内部でSubscribeの実行が走る
+        var disposable = appendStringObservable.Connect();
+
+        originalSubject.OnNext("I");
+        originalSubject.OnNext("have");
+
+        //appendStringObservableを直接Subscribeすればよい
+        appendStringObservable.Subscribe(x => Debug.Log(x));
+        originalSubject.OnNext("a");
+        originalSubject.OnNext("pen");
+        originalSubject.OnCompleted();
+
+        //Hot変換解除
+        disposable.Dispose();
+        originalSubject.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
