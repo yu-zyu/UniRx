a5d5469e4242a9b343cc532ba36ceb9a4b6a7fd7
yu-zyu <siragaigarasi@gmail.com>
2021-09-06 05:46:51 +0900

    MainThreadSchedulerを使うことと、メインスレッド上でCurrentThreadSchedulerを使う事の違い

---
+++ Assets/Example/Chapter2/2.4/2.4.4/CompareSchedulers/CompareSchedulers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section2.Schedulers
+{
+    public class CompareSchedulers : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Unityのコルーチンを用いて3秒計測してくれる（メインスレッドをブロックしない）
+            Observable
+                .Timer(TimeSpan.FromSeconds(3), Scheduler.MainThread)
+                .Subscribe(x => Debug.Log("a"))
+                .AddTo(this);
+
+            //メインスレッドをThread.Sleepして3秒計測
+            Observable
+                .Timer(TimeSpan.FromSeconds(3), Scheduler.CurrentThread)
+                .Subscribe(x => Debug.Log("b"))
+                .AddTo(this);
+        }
+    }
+}
