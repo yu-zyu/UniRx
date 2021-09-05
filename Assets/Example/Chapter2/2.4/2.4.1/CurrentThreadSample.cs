3c10b1e2797ce48644a8b0acbe9609df1eac1910
yu-zyu <siragaigarasi@gmail.com>
2021-09-06 04:25:23 +0900

    実行中のスレッドとは？を実装した

---
+++ Assets/Example/Chapter2/2.4/2.4.1/CurrentThreadSample.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section2.Schedulers
+{
+    public class CurrentThreadSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Unity Main Thread ID:" + Thread.CurrentThread.ManagedThreadId);
+
+            var subject = new Subject<Unit>();
+            subject.AddTo(this);
+
+            subject
+                //OnNextメッセージを現行スレッドにて処理する
+                //つまりそのまま素通しするのと変わらない
+                .ObserveOn(Scheduler.Immediate)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Thread Id:" + Thread.CurrentThread.ManagedThreadId);
+                });
+
+            //メインスレッドにてOnNext発行
+            subject.OnNext(Unit.Default);
+
+            //別スレッドからOnNextを発行
+            Task.Run(() => subject.OnNext(Unit.Default));
+
+            subject.OnCompleted();
+        }
+    }
+}
