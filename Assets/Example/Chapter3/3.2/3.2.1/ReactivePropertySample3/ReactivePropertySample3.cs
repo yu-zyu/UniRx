d0e581f683f1748ce1296f92e7239a22662d4804
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 06:03:47 +0900

    ReactiveProperty3を実装した

---
+++ Assets/Example/Chapter3/3.2/3.2.1/ReactivePropertySample3/ReactivePropertySample3.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.ReactiveProperty
+{
+    public class ReactivePropertySample3 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var health = new ReactiveProperty<int>(100);
+
+            health
+                //Subscribe()直後のOnNextメッセージを無視する
+                .SkipLatestValueOnSubscribe()
+                .Subscribe(x => Debug.Log("通知された値：" + x));
+
+            health.Dispose();
+        }
+    }
+}
