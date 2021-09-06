c86135c2321d2372b2aba0cf9864656d951f5970
yu-zyu <siragaigarasi@gmail.com>
2021-09-06 20:42:45 +0900

    ReplaySubjectを実装した

---
+++ Assets/Example/Chapter3/3.1/3.1.3/ReplaySubjectSample/ReplaySubjectSample.cs
@@ -0,0 +1,35 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Sample.Section3.Subjects
+{
+    public class ReplaySubjectSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //過去３メッセージまでキャッシュするReplaySubject
+            var subject = new ReplaySubject<int>(bufferSize: 3);
+
+            //メッセージを発行
+            for(int i=0; i < 10; i++)
+            {
+                subject.OnNext(i);
+            }
+
+            //OnCompletedメッセージもキャッシュされる
+            subject.OnCompleted();
+
+            //OnErrorメッセージもキャッシュできる
+            //subject.OnError(new Exception("Error!"));
+
+            //購読
+            subject.Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.LogError("OnError:" + ex),
+                () => Debug.Log("OnCompleted"));
+
+            subject.Dispose();
+        }
+    }
+}
