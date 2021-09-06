49420e38aa7bcca69f3294f06272f8f555c9ccd4
yu-zyu <siragaigarasi@gmail.com>
2021-09-06 13:01:58 +0900

    SubjectSampleを実装した

---
+++ Assets/Example/Chapter3/3.1/3.1.1/SubjectSample/SubjectSample.cs
@@ -0,0 +1,33 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Subjects
+{
+    public class SubjectSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var subject = new Subject<int>();
+
+            //メッセージを発行
+            subject.OnNext(1);
+
+            //購読開始
+            subject.Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.Log("OnError:" + ex),
+                () => Debug.Log("OnCompleted:")
+                );
+
+            //メッセージを発行
+            subject.OnNext(2);
+            subject.OnNext(3);
+
+            subject.OnCompleted();
+
+            //Dispose()を忘れない
+            subject.Dispose();
+        }
+    }
+}
