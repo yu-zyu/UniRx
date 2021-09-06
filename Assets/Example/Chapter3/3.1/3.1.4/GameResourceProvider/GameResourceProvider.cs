2212f3b978baa50900c315eb62edfaa1eddec65d
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 03:41:04 +0900

    GameResourceProviderを実装した

---
+++ Assets/Example/Chapter3/3.1/3.1.4/GameResourceProvider/GameResourceProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Subjects.Async
+{
+    //ゲームのリソースを読み込んで管理する
+    public class GameResourceProvider : MonoBehaviour
+    {
+        //プレイヤのテクスチャ情報を扱うAsyncSubject
+        private readonly AsyncSubject<Texture> _playerTextureAsyncSubject = new AsyncSubject<Texture>();
+
+        //プレイヤのテクスチャ情報
+        public IObservable<Texture> PlayerTextureAsync => _playerTextureAsyncSubject;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //起動時にテクスチャをロードする
+            StartCoroutine(LoadTexture());
+        }
+
+        //テクスチャを読み込むコルーチン
+        private IEnumerator LoadTexture()
+        {
+            //プレイヤのテクスチャを非同期で読み込み
+            var resource = Resources.LoadAsync<Texture>("Textures/player");
+
+            yield return resource;
+
+            //読み込みが完了したらAsyncSubjectで結果を通知する
+            _playerTextureAsyncSubject.OnNext(resource.asset as Texture);
+            _playerTextureAsyncSubject.OnCompleted();
+        }
+    }
+}
---
+++ Assets/Example/Chapter3/3.1/3.1.4/GameResourceProvider/PlayerTextureChanger.cs
@@ -0,0 +1,27 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Subjects.Async
+{
+    //プレイヤのテクスチャを変更する
+    public class PlayerTextureChanger : MonoBehaviour
+    {
+        [SerializeField]
+        private GameResourceProvider _gameResourceProvider;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //プレイヤのテクスチャの読み込みが完了次第テクスチャを変更する
+            _gameResourceProvider.PlayerTextureAsync
+                .Subscribe(SetMyTexture)
+                .AddTo(this);
+        }
+
+        private void SetMyTexture(Texture newTexture)
+        {
+            var r = GetComponent<Renderer>();
+            r.sharedMaterial.mainTexture = newTexture;
+        }
+    }
+}
