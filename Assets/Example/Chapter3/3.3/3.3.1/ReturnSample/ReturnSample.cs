414b223e34d87fe0a53aaf7d2cbf80dffd96cc69
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 20:57:51 +0900

    fix フォルダ構成

---
+++ Assets/Example/Chapter3/3.3/3.3.1/ReturnSample/ReturnSample.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class ReturnSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Return(100)
+                .Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.Log("OnError:" + ex),
+                () => Debug.Log("OnCompleted:")
+                );
+        }
+    }
+}
