6efb1693e96dacb4c77c4d1ce684b206a6d2dd7b
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 23:52:28 +0900

    RepeatSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.3/RepeatSample/RepeatSample.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class RepeatSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //"yes"を3個発行する
+            Observable.Repeat(value: "yes", repeatCount:3)
+                .Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.Log("OnError:" + ex),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
