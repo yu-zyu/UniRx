0baf622baddf453815d8442c4e9d0eea8f653583
yu-zyu <siragaigarasi@gmail.com>
2021-09-08 00:36:13 +0900

    DeferSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.7/DeferSample/DeferSample.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class DeferSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //「乱数を返すObservable」をSubsribe()されるたびに生成する
+            var rand = Observable.Defer(() =>
+            {
+                //乱数を用意
+                var randomValue = UnityEngine.Random.Range(0, 100);
+                //乱数を返すObservable
+                return Observable.Return(randomValue);
+            });
+
+            //複数回Subscribe()
+            rand.Subscribe(x => Debug.Log(x));
+            rand.Subscribe(x => Debug.Log(x));
+            rand.Subscribe(x => Debug.Log(x));
+        }
+    }
+}
