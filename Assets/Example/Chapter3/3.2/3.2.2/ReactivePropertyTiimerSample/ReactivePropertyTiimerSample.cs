c9f7589ded4de52f682dfb9113eba95ff6fcc8d6
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 19:40:06 +0900

    ReactivePropertyTimerSampleを実装した

---
+++ Assets/Example/Chapter3/3.2/3.2.2/ReactivePropertyTiimerSample/ReactivePropertyTiimerSample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.ReactiveProperty
+{
+    //カウントダウンするタイマ
+    public class ReactivePropertyTiimerSample : MonoBehaviour
+    {
+        //実態としてReactivePropertyを定義
+        [SerializeField]
+        private IntReactiveProperty _current = new IntReactiveProperty(60);
+
+        //現在のタイマの値（読み取り専用）
+        //ReactivePropertyをIRえあｄOnlyReactivePropertyにアップキャスト
+        public IReadOnlyReactiveProperty<int> CurrentTime => _current;
+
+        void Start()
+        {
+            StartCoroutine(CountDownCoroutine());
+            _current.AddTo(this);
+        }
+
+        private IEnumerator CountDownCoroutine()
+        {
+            while(_current.Value > 0)
+            {
+                //1秒に１つずつ値を更新する
+                _current.Value--;
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ Assets/Example/Chapter3/3.2/3.2.2/ReadOnlyReactivePropertySample/ReadOnlyReactivePropertySample.cs
@@ -0,0 +1,30 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.ReactiveProperty
+{
+    public class ReadOnlyReactivePropertySample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var intReactiveProperty = new ReactiveProperty<int>(100);
+
+            //int型のReadOnlyReactiveProperty
+            var readOnlyInt =
+                //ReactivePropertyからの変換
+                intReactiveProperty.ToReadOnlyReactiveProperty();
+
+            //Readできる
+            Debug.Log("現在の値:" + readOnlyInt.Value);
+
+            //購読できる
+            readOnlyInt.Subscribe(x => Debug.Log("通知された値：" + x));
+
+            //Writeは実行できない
+            //readOnlyInt.Value = 10;
+
+            intReactiveProperty.Dispose();
+        }
+    }
+}
