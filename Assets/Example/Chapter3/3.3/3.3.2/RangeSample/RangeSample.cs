cb4222f5daa6b81685047d46d5e91bfcc9b010a8
yu-zyu <siragaigarasi@gmail.com>
2021-09-07 21:04:30 +0900

    RangeSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.2/RangeSample/RangeSample.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class RangeSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //0から5個、連続した整数値を発行する
+            Observable.Range(start: 0, count: 5)
+                .Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.Log("OnError:" + ex),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
