4e756e5001bc07b72a1beb5b327efb064f6cd402
yu-zyu <siragaigarasi@gmail.com>
2021-09-08 00:16:21 +0900

    NeverSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.5/EmptySample/EmptySample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+
+    public class EmptySample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Empty<int>()
+                .Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.LogError("OnError:" + ex.Message),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
---
+++ Assets/Example/Chapter3/3.3/3.3.6/NeverSample/NeverSample.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class NeverSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Never<int>()
+                .Subscribe(
+                x => Debug.Log("OnNext:" + x),
+                ex => Debug.LogError("OnError:" + ex.Message),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
