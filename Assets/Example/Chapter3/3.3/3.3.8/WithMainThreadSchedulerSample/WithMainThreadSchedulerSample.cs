fc6e7c6eabac6f7a9b97e1e9c6835eee0cae60e5
yu-zyu <siragaigarasi@gmail.com>
2021-09-08 00:52:59 +0900

    WithMainThreadSchedulerSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.8/WithMainThreadSchedulerSample/WithMainThreadSchedulerSample.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class WithMainThreadSchedulerSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //MainThreadSchedulerを指定した場合
+            Observable.Range(
+                start: 0,
+                count: 5,
+                scheduler: Scheduler.MainThread
+                ).Subscribe(x =>
+                {
+                    //現在のフレーム数とメッセージ値を表示する
+                    Debug.Log($"frame:{Time.frameCount} value:{x}");
+                });
+        }
+    }
+}
