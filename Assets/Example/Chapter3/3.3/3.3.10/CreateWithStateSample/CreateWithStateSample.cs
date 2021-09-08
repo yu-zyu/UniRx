93d7949c12c42f5b2917ed0092c35ec4fe433213
yu-zyu <siragaigarasi@gmail.com>
2021-09-09 03:12:18 +0900

    CreateWithStateSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.10/CreateWithStateSample/CreateWithStateSample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class CreateWithStateSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            CreateCountObservable(10).Subscribe(x => Debug.Log(x));
+        }
+
+        //指定個数、連続した値を発行する（Rangeと同じ挙動）Observableを作って返す
+        private IObservable<int> CreateCountObservable(int count)
+        {
+            //countが0以下の場合はOnCompletedメッセージのみを返す
+            if (count <= 0) return Observable.Empty<int>();
+
+            //指定した数だけ連続する値を発行する
+            return Observable.CreateWithState<int, int>(
+                state: count,
+                subscribe: (maxCount, observer) =>
+                 {
+                    //maxCountにstateで指定した値が入っている
+                    for (int i = 0; i < maxCount; i++)
+                     {
+                         observer.OnNext(maxCount);
+                     }
+
+                     observer.OnCompleted();
+                     return Disposable.Empty;
+                 });
+        }
+    }
+}
