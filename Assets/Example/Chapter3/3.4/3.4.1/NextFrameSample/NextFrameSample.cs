35b64c36b61540c55330286cac1b008a5fe836dd
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 04:44:15 +0900

    NextFrameSampleを実装した

---
+++ Assets/Example/Chapter3/3.4/3.4.1/NextFrameSample/NextFrameSample.cs
@@ -0,0 +1,31 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class NextFrameSample : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //スペースキーが押されたら、Rigidbodyに力を加える
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //次のFixedUpdateのタイミングで処理を実行する
+                Observable.NextFrame(FrameCountType.FixedUpdate)
+                    .Subscribe(_ =>
+                    {
+                        _rigidbody.AddForce(Vector3.up, ForceMode.VelocityChange);
+                    });
+            }
+        }
+    }
+}
