7ca8fe35a173c3e7b21c71ffc7d7b59293523074
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 05:06:51 +0900

    EveryUpdateSampleを実装した

---
+++ Assets/Example/Chapter3/3.4/3.4.3/EveryUpdateSample/EveryUpdateSample.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.UnityEvents
+{
+    public class EveryUpdateSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //毎フレームオブジェクトの座標を更新する
+            Observable.EveryUpdate()
+                .Subscribe(_ =>
+                {
+                    transform.position += Vector3.forward * Time.deltaTime;
+                })
+                .AddTo(this); // GameObjectが破棄されたら確実に止める
+        }
+    }
+}
