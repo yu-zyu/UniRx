bc7f839ead2558a2732396dbec64f39f9b401f91
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 04:34:40 +0900

    UnityWebRequesToObservableを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.20/UnityWebRequestToObservable/UnityWebRequestToObservable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class UnityWebRequestToObservable : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //UniTaskからObservableに変換できる
+            FetchAsync("https://magical-iga.com/")
+                .ToObservable()
+                .Subscribe(x => Debug.Log(x));
+
+        }
+
+        //HTTP通信をUnityWebRequestで行う
+        private async UniTask<string> FetchAsync(string uri)
+        {
+            using(var uwr = UnityWebRequest.Get(uri))
+            {
+                //UniTaskを導入した場合はawaitできる
+                await uwr.SendWebRequest();
+                return uwr.downloadHandler.text;
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
