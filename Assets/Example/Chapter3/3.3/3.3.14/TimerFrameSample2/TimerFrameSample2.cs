b169744d050368e7975ce30d4874c73ad78cf936
yu-zyu <siragaigarasi@gmail.com>
2021-09-09 22:39:22 +0900

    TimerFrameSample2を実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.14/TimerFrameSample2/TimerFrameSample2.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class TimerFrameSample2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var timer = Observable.TimerFrame(dueTimeFrameCount: 5, periodFrameCount: 10);
+
+        Debug.Log("Subscribe()したフレーム：" + Time.frameCount);
+
+        timer.Subscribe(
+            x => Debug.Log($"{x} - {Time.frameCount}"),
+            () => Debug.Log("OnCompleted")
+            ).AddTo(this); //Dispose() の実行を忘れない
+    }
+}
