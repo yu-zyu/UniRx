ef8ea339526fae9d18d9e846bdae6b6e95460186
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 04:24:03 +0900

    ObservableWWSampleを実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.20/ObservableWWWSample/ObservableWWWSample.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class ObservableWWWSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var uri = "https://magical-iga.com/";
+
+            //結果（www.text)のみを受け取る場合
+            ObservableWWW.Get(uri)
+                .Subscribe(x => Debug.Log(x));
+
+            //通信終了時にwwwごと受け取る場合
+            ObservableWWW.GetWWW(uri)
+                .Subscribe(www => Debug.Log(www.text));
+        }
+    }
+}
