15964aee506bf049c4da8d975defc4486ec84810
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 03:43:46 +0900

    ToAsyncSample1を実装した

---
+++ Assets/Example/Chapter3/3.3/3.3.18/ToAsyncSample1/ToAsyncSample1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class ToAsyncSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Observable.ToAsyncの返り値はFunc<IObservable<T>>
+            Func<IObservable<string>> fileReadFunc;
+
+            //スレッドプール上でファイルを読み込む処理を実行する
+            fileReadFunc = Observable.ToAsync(() =>
+            {
+                using (var r = new StreamReader(@"data.txt"))
+                {
+                    //読み取り結果をObservableに流す
+                    return r.ReadToEnd();
+                }
+            }, Scheduler.ThreadPool);
+
+            //非同期処理を開始する為には明示的にメソッドを呼び出しする必要がある
+            fileReadFunc().Subscribe(x => Debug.Log(x));
+
+            //Invoke()でもよい
+            //fileReadFunc.Invoke().Subscribe();
+        }
+    }
+}
