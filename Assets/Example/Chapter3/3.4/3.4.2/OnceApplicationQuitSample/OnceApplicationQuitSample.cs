6ee5a745b2b3394ea99e062bb03d3604c16679ec
yu-zyu <siragaigarasi@gmail.com>
2021-09-10 05:00:25 +0900

    OnceApplicationQuitSampleを実装した

---
+++ Assets/Example/Chapter3/3.4/3.4.2/OnceApplicationQuitSample/OnceApplicationQuitSample.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class OnceApplicationQuitSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //OnApplicationQuitの実行タイミングで発火する
+            Observable.OnceApplicationQuit()
+                .Subscribe(_ => Debug.Log("アプリケーションが終了しました"));
+        }
+    }
+}
