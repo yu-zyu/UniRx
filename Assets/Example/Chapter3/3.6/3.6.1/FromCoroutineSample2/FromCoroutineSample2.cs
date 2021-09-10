5047af44696f281b7f789035a7f9999018c3c315
yu-zyu <siragaigarasi@gmail.com>
2021-09-11 07:27:17 +0900

    FromCoroutineSample3を実装した

---
+++ Assets/Example/Chapter3/3.6/3.6.1/FromCoroutineSample2/FromCoroutineSample2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Coroutines
+{
+    public class FromCoroutineSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //CancellationToken を利用する場合
+            Observable
+                .FromCoroutine(token => WaitingCoroutine(token))
+                .Subscribe(
+                _ => Debug.Log("OnNext"),
+                () => Debug.Log("OnCompleted"))
+                .AddTo(this);
+        }
+
+        //CancellationTokenを受け取る
+        private IEnumerator WaitingCoroutine(CancellationToken token)
+        {
+            Debug.Log("Coroutine start.");
+
+            //Observableをコルーチンとして待ち受ける場合,
+            //このコルーチンが停止したタイミングで
+            //yield return で待ち受けているObservableも止まってほしい
+            //そのためにCancellationTokenを用いる
+            yield return Observable
+                .Timer(TimeSpan.FromSeconds(3))
+                .ToYieldInstruction(token);
+
+            Debug.Log("Coroutine finish.");
+        }
+    }
+}
---
+++ Assets/Example/Chapter3/3.6/3.6.1/FromCoroutineSample3/FromCoroutineSample3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Coroutines
+{
+    public class FromCoroutineSample3 : MonoBehaviour
+    {
+        //長押し判定の閾値
+        private readonly float _longPressThresholdSeconds = 1.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //一定時間の長押しを検知するObservable
+            Observable.FromCoroutine<bool>(observer => LongPushCoroutine(observer))
+                .DistinctUntilChanged() //連続して重複したメッセージを除去
+                .Subscribe(x => Debug.Log(x)).AddTo(this);
+        }
+
+        //スペースキーの長押しを検知するコルーチン
+        //一定時間長押しされていたらTrueを返す
+        //キーが離されたらFalseを返す
+        private IEnumerator LongPushCoroutine(IObserver<bool> observer)
+        {
+            var isPushed = false;
+            var lastPushTime = Time.time;
+
+            while (true)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    if (!isPushed)
+                    {
+                        //押された直後の時刻を記憶
+                        lastPushTime = Time.time;
+                        isPushed = true;
+                    }
+                    else if(Time.time - lastPushTime > _longPressThresholdSeconds)
+                    {
+                        //一定時間押されているならTrueを発行
+                        observer.OnNext(true);
+                    }
+                }
+                else
+                {
+                    if (isPushed)
+                    {
+                        //離されたらFalseを発行
+                        observer.OnNext(false);
+                        isPushed = false;
+                    }
+                }
+
+            yield return null;
+            }
+        }
+    }
+}
