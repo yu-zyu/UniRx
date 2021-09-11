1cad2f9f1f3f1607a4bddde14120f6ec73590042
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 07:42:29 +0900

    LastSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.7/FirstOrDefaultSample2/FirstOrDefaultSample2.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class FirstOrDefaultSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //OnCompletedメッセージのみを発行する
+            Observable.Empty<int>()
+                .FirstOrDefault()
+                .Subscribe(
+                x => Debug.Log(x),
+                ex => Debug.LogError(ex),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
---
+++ Assets/Example/Chapter4/4.2/4.2.8/LastSample/LastSample.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class LastSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var array = new[] { 1, 3, 4, 7, 2, 5, 9 };
+
+            array
+                .ToObservable()
+                .Last()
+                .Subscribe(
+                x => Debug.Log(x),
+                ex => Debug.LogError(ex),
+                () => Debug.Log("OnCompleted"));
+        }
+    }
+}
