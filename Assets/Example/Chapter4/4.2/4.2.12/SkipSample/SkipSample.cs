158240b39051677d23029691b36ffde2e9d64427
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 08:00:55 +0900

    SkipSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.12/SkipSample/SkipSample.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class SkipSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //1オリジンで10個メッセージを生成
+            Observable.Range(1, 10)
+                //Subscribe()してから最初の3個を無視
+                .Skip(3)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
