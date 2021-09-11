288e95c2f3f1faedabea85d87a2d4abaa6426128
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 05:03:31 +0900

    DistinctSample2を実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.4/DistincrSample2/DistincrSample2.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class DistincrSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.OnCollisionEnterAsObservable()
+                //過去に衝突したことがあるGameObjectは無視
+                //Collision型からGameObjectのパラメータのみを取り出して比較する
+                .Distinct(x => x.gameObject)
+                .Subscribe(x => Debug.Log(x.gameObject.name));
+        }
+    }
+}
