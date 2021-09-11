d6d4481eb1b12b7bdd564c63a04c0467edd54995
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 07:48:39 +0900

    SingleSample1を実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.10/SingleSample1/SingleSample1.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class SingleSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Range(1, 10)
+                //5のみを通過する
+                .Single(x => x == 5)
+                .Subscribe(
+                x => Debug.Log(x),
+                ex => Debug.LogError(ex),
+                () => Debug.Log("OnCompleted"));
+        }
+    }
+}
