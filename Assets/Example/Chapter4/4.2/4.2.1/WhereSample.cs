bf123c9c652edda5aa940ac6e0f6cbf98305d57d
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 04:14:00 +0900

    WhereSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.1/WhereSample.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class WhereSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Range(0, 10)
+                .Where(x => x % 2 == 0) //偶数のみを許可する
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
