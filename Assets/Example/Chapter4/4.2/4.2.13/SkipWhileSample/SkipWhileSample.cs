29eb9669a968c61f83e8226cf590814ed41c48ae
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 08:08:25 +0900

    SkipWhileSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.13/SkipWhileSample/SkipWhileSample.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class SkipWhileSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var array = new[] { 1, 3, 4, 7, 2, 5, 9 };
+
+            array.ToObservable()
+                //数値が5より小さい間は無視する
+                .SkipWhile(x => x < 5)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
