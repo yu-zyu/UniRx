de07a0e6ad3deb9c086241dc2b0c1773167ddb1b
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 06:27:27 +0900

    FistSample2を実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.6/FirstSample2/FirstSample2.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class FirstSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //OnCompletedメッセージのみを発行する
+            Observable.Empty<int>()
+                .First()
+                .Subscribe(
+                x => Debug.Log(x),
+                ex => Debug.LogError(ex),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
