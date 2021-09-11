8cde1c236541614e5f14ffb753d53d2553f2274b
yu-zyu <siragaigarasi@gmail.com>
2021-09-11 22:14:58 +0900

    YieldInstrunctionSample1を実装した

---
+++ Assets/Example/Chapter3/3.6/3.6.1/FromCoroutineValueSample/FromCoroutineValueSample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Coroutines
+{
+    public class FromCoroutineValueSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.FromCoroutineValue<Vector3>(PositionCoroutine, false)
+                .Subscribe(
+                x => Debug.Log(x),
+                () => Debug.Log("OnCompleted"));
+        }
+
+        //座標の配列を順番に発行するイテレータ
+        private IEnumerator PositionCoroutine()
+        {
+            var positions = new[]
+            {
+                new Vector3(0,0,0),
+                new Vector3(0,1,0),
+                new Vector3(0,1,1),
+                new Vector3(1,1,1),
+            };
+
+            foreach(var p in positions)
+            {
+                yield return p;
+            }
+        }
+    }
+}
---
+++ Assets/Example/Chapter3/3.6/3.6.2/YieldInstructionSample1/YieldInstructionSample1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section3.Coroutines
+{
+    public class YieldInstructionSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(WaitCoroutine());
+        }
+
+        private IEnumerator WaitCoroutine()
+        {
+            Debug.Log("Coroutine start:" + Time.time);
+
+            //Observable をコルーチンに変換する
+            yield return Observable
+                .Timer(TimeSpan.FromSeconds(1))
+                .ToYieldInstruction();
+
+            Debug.Log("Coroutine end:" + Time.time);
+        }
+    }
+}
