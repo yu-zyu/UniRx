f7712ffd1d8653dcf2bccdbf9fbcc2b5a659c57c
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 06:22:39 +0900

    FistSample1を実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.5/DistinctUntilChangedSample/DistinctUntilChangedSample.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class DistinctUntilChangedSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var array = new[] { 1, 2, 2, 3, 1, 1, 2, 2, 2, 3 };
+
+            array.ToObservable()
+                .DistinctUntilChanged()
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
