6f07f8de45ce40f614996e003358cdd766b218a2
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 20:44:31 +0900

    TakeUntilDestroyを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.17/TakeUntil/TakeUntil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class TakeUntil : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //このGameObjectがDestroyされるまでｶｳﾝﾄｱｯﾌﾟし続ける
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .TakeUntil(this.OnDestroyAsObservable())
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
---
+++ Assets/Example/Chapter4/4.2/4.2.18/TAkeUnitilDestroySample/TAkeUnitilDestroySample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class TAkeUnitilDestroySample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //このGameObjectがDestroyされるまでカウントアップし続ける
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .TakeUntilDestroy(gameObject)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
