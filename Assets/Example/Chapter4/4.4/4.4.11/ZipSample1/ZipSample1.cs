9b84e72fa1336f0218d004db6758f2f69339f7af
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 04:05:16 +0900

    ZipSample2を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.11/ZipSample1/ZipSample1.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class ZipSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var stream1 = new[] { "A", "B", "C", "D" }.ToObservable();
+            var stream2 = Observable.Range(0, 3).Select(x => x);
+
+            stream1
+                .Zip(stream2, (x1, x2) => $"{x1}:{x2}")
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
