8b76d1b9697766e6e2a49c24b3aa5a2e24bd38d1
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 23:41:03 +0900

    MergeSample2を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.1/MergeSample2/MergeSample2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class MergeSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Observableを扱う Observable
+            IObservable<IObservable<int>> streams =
+                Observable.Range(1, 3, Scheduler.Immediate)
+                .Select(x =>
+                {
+                    return Observable.Range(x * 100, 3, Scheduler.Immediate);
+                });
+            streams
+                .Merge() // 1つのObservableにまとめる
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
