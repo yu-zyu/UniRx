f319de1b3bd4815b2726da96416e0117ee07c319
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 02:54:54 +0900

    BatchFrameSample1を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.8/BufferSample2/BufferSample2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class BufferSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //マウスクリックイベント
+            var mouseDown =
+                this.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0));
+            mouseDown
+                //マウスクリックイベントをバッファリングする
+                //バッファの解放条件は
+                //「最後にクリックされてから500msの間マウスクリックがなかったとき」
+                .Buffer(mouseDown.Throttle(TimeSpan.FromMilliseconds(500)))
+                //クリックされた回数でフィルタリング
+                .Where(x => x.Count == 2)
+                .Subscribe(_ => Debug.Log("ダブルクリックされました"));
+        }
+    }
+}
---
+++ Assets/Example/Chapter4/4.4/4.4.9/BatchFrameSample1/BatchFrameSample1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class BatchFrameSample1 : MonoBehaviour
+    {
+        //敵オブジェクトの管理をしているコンポーネント（という想定）
+        [SerializeField] private EnemyManager _enemyManager = new EnemyManager();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //倒した敵のIDがObservableで通知される（という想定）
+            IObservable<int> killed = _enemyManager.OnKilledEnemyIdObservable;
+
+            //フレーム数0,FrameCountType.EndOfFrameを指定する事で、
+            //このフレーム中に発生したイベントを１つにまとめられる
+            killed
+                .BatchFrame(0, FrameCountType.EndOfFrame)
+                .Subscribe(ids =>
+                {
+                    //1フレーム中に倒した敵の数を出力する
+                    Debug.Log(Time.frameCount + ":" + ids.Count);
+                });
+        }
+    }
+
+    public class EnemyManager
+    {
+        private Subject<int> enemyId = new Subject<int>();
+
+        public IObservable<int> OnKilledEnemyIdObservable => enemyId;
+    }
+}
