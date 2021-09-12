36f44e653920fdf691ece181ef582dbc784d9f18
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 09:51:05 +0900

    TakeSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.14/SkipUntilSample/SkipUntilSample.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class SkipUntilSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //カメラに描画されると発行されるイベント
+            var onBecameVisible = this.OnBecameVisibleAsObservable();
+
+            //カメラに描画されたタミングから移動を開始する
+            this.UpdateAsObservable()
+                .SkipUntil(onBecameVisible)
+                .Subscribe(_ =>
+                {
+                    transform.position += Vector3.forward * Time.deltaTime;
+                });
+        }
+    }
+}
