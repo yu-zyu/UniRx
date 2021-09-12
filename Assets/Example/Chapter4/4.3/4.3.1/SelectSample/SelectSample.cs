e5418bcb61aaed6dbc3286de8c4d29cd8bfbd717
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 22:58:26 +0900

    SelectSampleを実装した

---
+++ Assets/Example/Chapter4/4.3/4.3.1/SelectSample/SelectSample.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Converters
+{
+    public class SelectSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Range(1, 5)
+                //メッセージの内容を10倍する
+                .Select(x => x * 10)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
