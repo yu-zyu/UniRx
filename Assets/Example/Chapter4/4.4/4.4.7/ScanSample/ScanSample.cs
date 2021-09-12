4449dc9e38f60db5a40c4d3b42f573509c897648
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 01:36:45 +0900

    ScanSampleを実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.7/ScanSample/ScanSample.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class ScanSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //発行された値を順次足し合わせながら出力する
+            Observable.Range(1,5)
+                .Scan(0, (pre, cur) => pre + cur)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
