75e439bb462b2ae719a77f166aac055c3be6f413
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 03:07:18 +0900

    BatchFrameSample2を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.9/BatchFrameSample2/BatchFrameSample2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class BatchFrameSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var rigidBody = GetComponent<Rigidbody>();
+
+            //ジャンプボタンが入力されたら通知されるObservable
+            IObservable<Unit> jumpButton =
+                this.UpdateAsObservable()
+                .Where(_ => Input.GetButtonDown("Jump"));
+            //ジャンプボタンがおされたら次のFixedUpdateでジャンプ処理をする
+            jumpButton
+                .BatchFrame(0, FrameCountType.FixedUpdate)
+                .Subscribe(_ =>
+                {
+                    rigidBody.AddForce(Vector3.up, ForceMode.VelocityChange);
+                });
+        }
+    }
+}
