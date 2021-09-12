d98c58eec3015c3725a03d265d5fafeb436d6cbc
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 23:55:22 +0900

    ConcatSampleを実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.2/ConcatSample1/ConcatSample1.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class ConcatSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var s1 = Observable.Range(0, 3);
+            var s2 = Observable.Range(10, 3);
+
+            s1.Concat(s2)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
