2677474c33f07e66d0b5b5ab79b148b603137dd0
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 22:31:57 +0900

    ThrottleSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.21/ThrottleSample/ThrottleSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class ThrottleSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //GameObjectが静止するまで待ってからその座標を取り出す
+            transform
+                .ObserveEveryValueChanged(x => x.position)
+                .Throttle(TimeSpan.FromSeconds(1)) //1秒
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
