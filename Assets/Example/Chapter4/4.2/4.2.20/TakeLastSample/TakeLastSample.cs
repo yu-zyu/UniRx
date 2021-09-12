baaeea8682a591ec624f020d9bc14222abdd437f
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 20:50:44 +0900

    TakeLastSampleを実装した

---
+++ Assets/Example/Chapter4/4.2/4.2.20/TakeLastSample/TakeLastSample.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Filters
+{
+    public class TakeLastSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var array = new[] { 1, 3, 4, 7, 2, 5, 9 };
+
+            array.ToObservable()
+                //最後から3つ取り出す
+                .TakeLast(3)
+                .Subscribe(
+                x => Debug.Log(x),
+                () => Debug.Log("OnCompleted")
+                );
+        }
+    }
+}
