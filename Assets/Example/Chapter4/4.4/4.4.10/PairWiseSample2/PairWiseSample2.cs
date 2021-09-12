b2a0860f8b3e1201f51bca0266d3ee47d74e1926
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 03:23:57 +0900

    PairWiseSample2を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.10/PairWiseSample2/PairWiseSample2.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class PairWiseSample2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //移動したときにその差分を計算する
+            transform.ObserveEveryValueChanged(x => x.position)
+                .Pairwise((p, c) => c - p)
+                .Subscribe(x => Debug.Log("移動した距離：" + x));
+        }
+    }
+}
