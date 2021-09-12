ee4f8351bf1ebfcb53dc47426946240bbde42817
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 23:49:06 +0900

    MergeSample3を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.1/MergeSample3/MergeSample3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class MergeSample3 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            IEnumerable<IObservable<int>> streams = new[]
+            {
+                Observable.Range(100, 3),
+                Observable.Range(200, 3),
+                Observable.Range(300, 3),
+            };
+
+            streams
+                .Merge() // 配列内のObservableを1つのObservableにまとめる
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
