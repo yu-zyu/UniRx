30612f351a7f595716a6ef49255bcea9a3a5e76e
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 01:31:37 +0900

    AggregateSampleを実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.6/AggregateSample/AggregateSample.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class AggregateSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //発行された値をすべて足し合わせる
+            Observable.Range(0, 10)
+                .Aggregate(0, (pre, cur) => pre + cur)
+                .Subscribe(x => Debug.Log(x));
+        }
+    }
+}
