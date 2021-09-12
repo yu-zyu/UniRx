b961fe180a1a0d795b48ac218e5b5127fec1c45f
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 03:17:30 +0900

    PairWiseSample1を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.10/PairWiseSample1/PairWiseSample1.cs
@@ -0,0 +1,19 @@
+﻿using UniRx;
+using UnityEngine;
+namespace Samples.Section4.Synthesizers
+{
+    public class PairWiseSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Range(0, 5) // 0,1,2,3,4
+                .Pairwise()
+                .Subscribe(p =>
+                {
+                    //Current と Previousにそれぞれの値が入っている
+                    Debug.Log($"{p.Previous}:{p.Current}");
+                });
+        }
+    }
+}
