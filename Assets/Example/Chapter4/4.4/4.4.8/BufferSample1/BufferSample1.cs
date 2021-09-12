12f54a41bea9bd1d74a8afb5f1f1ae66ddbdf2eb
yu-zyu <siragaigarasi@gmail.com>
2021-09-13 01:48:03 +0900

    BufferSample1を実装した

---
+++ Assets/Example/Chapter4/4.4/4.4.8/BufferSample1/BufferSample1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section4.Synthesizers
+{
+    public class BufferSample1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Observable.Range(1, 6)
+                //値を1つ前のものとセットにする
+                .Buffer(2, 1)
+                .Subscribe(x =>
+                {
+                    if (x.Count > 1)
+                    {
+                        Debug.Log(x[0] + ":" + x[1]);
+                    }
+                    else
+                    {
+                        Debug.Log(x[0]);
+                    }
+                });
+        }
+    }
+}
