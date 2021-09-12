fead3811b9068ce351bb9a9f15c2ca32a7917a25
yu-zyu <siragaigarasi@gmail.com>
2021-09-12 23:10:17 +0900

    CastSampleを実装した

---
+++ Assets/Example/Chapter4/4.3/4.3.2/CastSample/CastSample.cs
@@ -0,0 +1,31 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Samples.Section4.Converters
+{
+    public class CastSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var objects = new object[]
+            {
+                "hoge",
+                "fuga",
+                'a',
+                -1,
+                "fuga",
+                'Z',
+                0.1
+            };
+
+            objects.ToObservable()
+                //string型にcastする
+                .Cast<object, string>()
+                .Subscribe(
+                x => Debug.Log(x),
+                ex => Debug.LogError(ex),
+                () => Debug.Log("OnCompleted"));
+        }
+    }
+}
