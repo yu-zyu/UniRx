19b5d8e913494a69c54b777347f1ae0e4d85c6ee
yu-zyu <siragaigarasi@gmail.com>
2021-09-15 00:52:25 +0900

    CatchSample2を実装した

--- Assets/Example/Chapter4/4.6/4.6.1/CatchSample2/CatchSample2.cs
+++ Assets/Example/Chapter4/4.6/4.6.1/CatchSample2/CatchSample2.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CatchSample2 : MonoBehaviour
+namespace Samples.Section4.ErrorHandlers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CatchSample2 : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private string[] _serverUrls;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Start()
+        {
+            // 複数のURIを先頭から読み込み、正常に読み込めた時点で終了
+            // 失敗した場合は次のURIに切り替えて読み込み直す
+            FetchTextDataAsync(_serverUrls)
+                .Subscribe(x => { Debug.Log(x); });
+        }
+
+        /// <summary>
+        /// データをサーバから読み込んで非同期で返す
+        /// </summary>
+        private IObservable<string> FetchTextDataAsync(string[] uris)
+        {
+            // URI一覧を変換
+            IObservable<string>[] observables = uris
+                // この Select はLINQ
+                .Select(x => Observable.Defer(() => FetchAsObservable(x)))
+                .ToArray();
+
+            // URIを先頭から読み込んでいく
+            return observables.Catch();
+        }
+
+        /// <summary>
+        /// サーバから読み取る
+        /// </summary>
+        private IObservable<string> FetchAsObservable(string uri)
+        {
+            return FetchAsync(uri).ToObservable();
+        }
+
+        /// <summary>
+        /// HTTP通信をUnityWebRequestで行う
+        /// </summary>
+        private async UniTask<string> FetchAsync(string uri)
+        {
+            using (var uwr = UnityWebRequest.Get(uri))
+            {
+                // UniTaskを導入した場合はawaitができる
+                await uwr.SendWebRequest();
+
+                if (uwr.isNetworkError || uwr.isHttpError)
+                {
+                    throw new Exception($"Error>{uwr.error}");
+                }
+
+                return uwr.downloadHandler.text;
+            }
+        }
     }
 }
