faf2cc26c0fda85c49b25bacff04a77d05f77156
yu-zyu <siragaigarasi@gmail.com>
2021-09-16 04:28:37 +0900

    DelaySubscriptionSampleを実装した

--- Assets/Example/Chapter4/4.8/4.8.5/DelaySubscriptionSample/DelaySubscriptionSample.cs
+++ Assets/Example/Chapter4/4.8/4.8.5/DelaySubscriptionSample/DelaySubscriptionSample.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using UniRx;
 using UnityEngine;
 
-public class DelaySubscriptionSample : MonoBehaviour
+namespace Samples.Section4.Schedulers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DelaySubscriptionSample : MonoBehaviour
     {
-        
-    }
+        private void Start()
+        {
+            Observable.Return(1)
+                .DoOnSubscribe(() =>
+                {
+                    Debug.Log("実際にSubscribeされた時間:" + Time.time);
+                })
+                .DelaySubscription(TimeSpan.FromSeconds(1))
+                .Subscribe();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log("Subscribeを呼び出した時間:" + Time.time);
+        }
     }
 }
